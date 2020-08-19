0d8a000337adb29003c3fee3f9757e748f82d721
Dr. A K M Anisur Rahman <arahman_med@bau.edu.bd>
2020-08-19 22:05:37 +0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,99 @@
+#setting working directory
+
+setwd("C:/Users/Medicine/Desktop/Data cleaning")
+
+library(dplyr)
+
+filename <- "Smartphonedat.zip"
+
+# Checking if filename already exists.
+if (!file.exists(filename)){
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, filename, mode = "wb")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+        unzip(filename) 
+}
+
+list.files("UCI HAR Dataset")
+
+##Making the Test and Training Set Data
+
+#Reading the training data
+
+subjectrain <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+
+xtrain <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+
+ytrain <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+#Reading the test data
+
+subjectest <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+
+xtest <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+
+ytest <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+
+#Reading features data
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+
+#Reading activity labels data
+
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
+##1. Merging the training and the test sets to create one data set
+Xtotal <- rbind(xtrain, xtest)
+Ytotal <- rbind(ytrain, ytest)
+Subtotal <- rbind(subjectrain, subjectest)
+Mergedata<-cbind(Subtotal,Xtotal,Ytotal)
+
+#2. Extracting the measurements on the mean and standard deviation for each measurement
+MeanSd <- Mergedata %>% select(subject, code, contains("mean"), contains("std"))
+
+MeanSd
+
+#3. Using descriptive activity names to name the activities in the data set
+
+Activitylabel<- activities[Mergedata$code, 2]
+
+#4. Appropriately labels the data set with descriptive variable names
+
+names(Mergedata)[2] = "activity"
+
+names(Mergedata)<-gsub("Acc", "Accelerometer", names(Mergedata))
+
+names(Mergedata)<-gsub("Gyro", "Gyroscope", names(Mergedata))
+
+names(Mergedata)<-gsub("BodyBody", "Body", names(Mergedata))
+
+names(Mergedata)<-gsub("Mag", "Magnitude", names(Mergedata))
+
+names(Mergedata)<-gsub("^t", "Time", names(Mergedata))
+
+names(Mergedata)<-gsub("^f", "Frequency", names(Mergedata))
+
+names(Mergedata)<-gsub("tBody", "TimeBody", names(Mergedata))
+
+names(Mergedata)<-gsub("-mean()", "Mean", names(Mergedata), ignore.case = TRUE)
+
+names(Mergedata)<-gsub("-std()", "STD", names(Mergedata), ignore.case = TRUE)
+
+names(Mergedata)<-gsub("-freq()", "Frequency", names(Mergedata), ignore.case = TRUE)
+
+names(Mergedata)<-gsub("angle", "Angle", names(Mergedata))
+
+names(Mergedata)<-gsub("gravity", "Gravity", names(Mergedata))
+
+#5. Creates a second, independent tidy data set from step 4 with the average of each variable for each activity and each subject
+
+Tidydata <- Mergedata %>%
+            group_by(subject, activity) %>%
+            summarise_all(funs(mean))
+write.table(Tidydata, "Tidydata.txt", row.name=FALSE)
+
+str(Tidydata)
+Tidydata
